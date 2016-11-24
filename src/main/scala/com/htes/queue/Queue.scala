036fc5b02a10824a437e42227d6f6db1e2f87623
Ara <aratatous@gmail.com>
2016-11-23 20:42:37 -0800

    Added Array-based queue.

---
+++ src/main/scala/com/htes/queue/Queue.scala
@@ -0,0 +1,47 @@
+package com.htes.queue
+
+/**
+  * This is a generic array based circular buffer queue.
+  * The capacity of this queue is size - 1.
+  * Int.MaxValue denotes invalid value.
+  *
+  */
+class Queue(size : Int) {
+  /** Points to the head, the oldest item added */
+  var head = 0
+
+  /** Points to the tail, the newest item added */
+  var tail = 0
+
+  /** Points to the actual buffer  */
+  val buff = new Array[Int](size)
+
+  def isEmpty = head == tail
+
+  def isFull =  head == (tail + 1) % buff.length
+
+  def insert(t : Int) {
+    if (!isFull) {
+      buff(tail) = t.asInstanceOf[Int];
+      tail = (tail + 1) % buff.length
+    }
+  }
+
+  def delete : Int = {
+    var num : Int = Int.MaxValue
+    if (!isEmpty) {
+      num = buff(head)
+      buff(head) = Int.MaxValue
+      head = (head + 1) % buff.length
+    }
+    num
+  }
+
+  def display {
+    System.out.print("head=" + head + ", tail=" + tail + "\nBuffer : ");
+    for (i <- head until tail) {
+      print(s"${buff(i)}, ");
+    }
+    println();
+  }
+}
