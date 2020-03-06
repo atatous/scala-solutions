50cca7ea0b039608ab26ade77277df9728226a64
Ara Tatous <aratatous@gmail.com>
2020-03-05 20:43:58 -0800

    Update QuickSort2.scala

--- src/main/scala/com/htes/sort/QuickSort2.scala
+++ src/main/scala/com/htes/sort/QuickSort2.scala
@@ -4,14 +4,14 @@ object QuickSort2 extends App {
 
   def qsort(a :Array[Int]) :Array[Int] = {
     if(a.length < 2) a else {
-      val p = a.length-1
-      val s = for (i <- a.take(p) if(i <= a(p))) yield i
-      val l = for (i <- a.take(p) if(i > a(p))) yield i
-      qsort(s) ++ Array(a(p)) ++ qsort(l)
+      val s = for(i <- a.tail if(i <= a(0))) yield i
+      val l = for(i <- a.tail if(i > a(0))) yield i
+      qsort(s) ++ Array(a(0)) ++ qsort(l)
     }
   }
 
-  val array = Array(5, 3, 4, 1, 2, 12, 25, 0, 17,13, 15, 10)
+  val array = Array(5, 3, 4, 1, 2, 12, 25, 0, 17,13, 15, 10, 2, 5)
+
   println(array.mkString(","))
   println(qsort(array).mkString(","))
 }
