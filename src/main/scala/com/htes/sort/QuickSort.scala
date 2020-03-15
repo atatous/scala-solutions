02cc4f2e776b692f518d16c8b4c675e0aaa349d0
Ara Tatous <aratatous@gmail.com>
2020-03-14 22:17:47 -0700

    Add files via upload

--- src/main/scala/com/htes/sort/QuickSort.scala
+++ src/main/scala/com/htes/sort/QuickSort.scala
@@ -1,40 +1,42 @@
 package com.htes.sort
 
-import scala.collection.mutable.ArrayBuffer
+object NonRecursiveQuickSort extends App {
 
-object QuickSort {
-
-  def swap(nums  :ArrayBuffer[Int], p :Int, q :Int)= {
+  def swap(nums: Array[Int], p: Int, q: Int) = {
     val t = nums(p)
     nums(p) = nums(q)
     nums(q) = t
   }
 
-  def partition(nums  :ArrayBuffer[Int], p :Int, q :Int) :Int = {
-    var j = p -1
-    for(i <- p until q)
-      if(nums(i) <= nums(q)) {
+  def partition(nums: Array[Int], p: Int, q: Int): Int = {
+    var j = p - 1
+    for (i <- p until q)
+      if (nums(i) <= nums(q)) {
         j += 1
         swap(nums, i, j)
       }
-    swap(nums, j+1, q)
-    j+1
+    swap(nums, j + 1, q)
+    j + 1
   }
 
-  def sort(nums  :ArrayBuffer[Int], p :Int, q :Int) :Unit = {
-    if (p<q) {
-      val r = partition(nums, p, q)
-      sort(nums, p, r - 1)
-      sort(nums, r + 1, q)
+  def qsort(a: Array[Int]): Array[Int] = {
+    var p = 0
+    var r = a.length - 1
+    while (p < r) {
+      val q = partition(a, p, r)
+      r = q - 1
     }
+    p = 0
+    r = a.length - 1
+    while (p < r) {
+      val q = partition(a, p, r)
+      p = q + 1
+    }
+    a
   }
 
-  def main(args :Array[String]) :Unit = {
-    val ints = ArrayBuffer(50,4,3,13,22,34,12,11,43,45,23,50)
-    println(s"Before: $ints")
-    sort(ints, 0, ints.length-1)
-    println(s"After: $ints")
-  }
-
-
+  val ints = Array(50,4,3,13,22,34,12,11,43,45,23,50)
+  println(ints.mkString(","))
+  qsort(ints)
+  println(ints.mkString(","))
 }
